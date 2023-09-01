d6bd2785e5c0259e483a54889bb6ebc0451d39be
Ahyana A <aatibada@gmail.com>
2023-09-01 10:51:49 -0400

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import desktopIllustration from "./images/illustration-sign-up-desktop.svg";
+import mobileIllustration from "./images/illustration-sign-up-mobile.svg";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Subscribe />
     </div>
   );
+}
+
+function Subscribe() {
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  return (
+    <main>
+      <section className="content-container">
+        <h1>Stay updated!</h1>
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        <ul>
+          <li>
+            <span className="list-space"></span>Product discovery and building
+            what matters
+          </li>
+          <li>
+            <span className="list-space"></span>Measuring to ensure updates are
+            a success
+          </li>
+          <li>
+            <span className="list-space"></span>And much more!
+          </li>
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="email@company.com"
+            pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+            required
+          />
+          <button type="submit">Subscribe to monthly newsletter</button>
+        </form>
+      </section>
+      <section className="img-container">
+        <img
+          clasName="desktop-illustration"
+          src={desktopIllustration}
+          alt="Computer illustration"
+        />
+        <img
+          src={mobileIllustration}
+          alt="Computer illustration"
+          className="mobile-illustration"
+        />
+      </section>
+    </main>
+  );
+}
+
+function Success() {
+  return (
+    <div>
+      Success!
+    </div>
+  )
 }
 
 export default App;
